e6a8ca0e31684c55978b2ec6ad55c53adcb24583
Parth Agrawal <86774143+Itsparththing@users.noreply.github.com>
2024-11-05 11:57:56 +0530

    Update variable.tf

--- redinfracraft/Terraform/Phishing/variable.tf
+++ redinfracraft/Terraform/Phishing/variable.tf
@@ -5,5 +5,5 @@ variable "key_name" {
 
 variable "security_group" {
   description = "Security group for EC2 Instance creatted through Terraform."
-  default = "Terra_Pwn_Ec2_sg"
+  default = "Terra_Phish_Ec2_sg"
 }
